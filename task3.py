cf30819b352db397822de3004d7fc9e73349ecde
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-01-28 16:14:33 -0800

    implemented fast d calculation through extended euclidian algo, still bugged

--- task3.py
+++ task3.py
@@ -8,31 +8,53 @@ def dh_proc(base, exp, mod):
         ans = (ans * base) % mod 
     return ans
 
+
+# use extended euclidian algo to find x, y s.t.
+# ax + by = gcd(a, b); a > b
+# returns x, y
+def extEuclidAlg(a, b):
+    remainder = a % b
+    s_prev, s = 0, 1
+    t_prev, t = 1, - (a//b)
+    while remainder != 0:
+        a = b
+        b = remainder
+        s_temp = s
+        t_temp = t
+        s = s_prev - s*(a//b)
+        t = t_prev - t*(a//b)
+        s_prev = s_temp
+        t_prev = t_temp
+        remainder = a % b
+    return (s_prev, t_prev)
 # finds mod multiplicative inverse of num mod mod
 def findInverse(num, mod):
     for i in range(mod):
         if (i * num) % mod == 1:
             return i
     return None
+
+
     
 def test():
-    p = getPrime(2048) # 2048 bit primes
-    q = getPrime(2048)
+    p = getPrime(8) # 2048 bit primes
+    q = getPrime(8)
     n = p * q
     euler_totient = (p-1) * (q-1)
 
     pub_key = (E, n)
-    d = findInverse(E, euler_totient)
+    d = abs(extEuclidAlg(E, euler_totient)[0])
     pri_key = (d, n)
 
-    plaintext = "hello world"
+    plaintext = "h"
     pt_as_number = int.from_bytes(plaintext.encode('utf-8'), "big")
 
-    ciphertext = dh_proc(plaintext, E, n)
+    ciphertext = dh_proc(pt_as_number, E, n)
     print("ciphertext:", ciphertext)
 
     pt_decrypt = dh_proc(ciphertext, d, n)
     pt_bytes = pt_decrypt.to_bytes((pt_as_number.bit_length() + 7) // 8, 'big')
     print(pt_bytes.decode('utf-8'))
 
-test()
+while(True):
+    test()
