41590ceb6f9260a9ca48d8aa198f5a808ed11d5b
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-01-29 17:44:36 -0800

    Finish lab
    Co-authored-by: Tony Li <LiTony@users.noreply.github.com>

--- task3.py
+++ task3.py
@@ -1,12 +1,8 @@
 from Crypto.Util.number import getPrime
-import sys
+from Crypto.Cipher import AES
+import sys, hashlib
+from task2 import pad_128_bits
 E=65537
-# diffie-hellman procedure, calculates base^exp % mod
-def dh_proc(base, exp, mod):
-    ans = 1
-    for i in range(exp):
-        ans = (ans * base) % mod 
-    return ans
 
 # https://en.wikipedia.org/wiki/Exponentiation_by_squaring 
 # fast exponentiation by squares
@@ -21,33 +17,16 @@ def fastExp(base, exp, mod):
         half = fastExp(base, exp//2, mod)
         return (half * half) % mod
 
-# https://en.wikipedia.org/wiki/Euclidean_algorithm#Procedure
+# https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 # use extended euclidian algo to find x, y s.t.
 # ax + by = gcd(a, b); a > b
-# returns x, y
-def extEuclidAlg(a, b):
-    remainder = a % b
-    s_prev, s = 0, 1
-    t_prev, t = 1, - (a//b)
-    while remainder != 0:
-        a = b
-        b = remainder
-        s_temp = s
-        t_temp = t
-        s = s_prev - s*(a//b)
-        t = t_prev - t*(a//b)
-        s_prev = s_temp
-        t_prev = t_temp
-        remainder = a % b
-    return (s_prev, t_prev)
-
+# returns mod inverse
 def tableAlg(a, b):
     r0,r1 = a, b
     s0,s1 = 1, 0
     t0,t1 = 0, 1
     #start of recursion
     ans = tableHelper(0, r0, r1, s0, s1, t0, t1)
-    print(ans)
     if (ans[4] < 0):
         return ans[3]
     return ans[3] + ans[4]
@@ -64,14 +43,8 @@ def tableHelper(q, rN, rN1, sN, sN1, tN, tN1):
         newT = tN1
         newTN1 = tN - newQ * tN1
         return tableHelper(newQ, newR, newRN1 , newS, newSN1, newT, newTN1)
-
-def findInverse(num, mod):
-    for i in range(mod):
-        if (i * num) % mod == 1:
-            return i
-    return None
     
-def test():
+def part1():
     print("generating primes...")
     p = getPrime(2048) # 2048 bit primes
     q = p
@@ -88,69 +61,48 @@ def test():
     plaintext = "hello world"
     pt_as_number = int.from_bytes(plaintext.encode('utf-8'), "big")
 
-    ciphertext = fastExp(pt_as_number, E, n) #4096 -> 4096 13^65537 mod n --> ct <= n
-    print(d.bit_length())
-    pt_decrypt = fastExp(ciphertext, d, n) #(2^4096)^(2^4096) (13^65537)^(2^4096) => 2^4096 operations
+    ciphertext = fastExp(pt_as_number, E, n) 
+    pt_decrypt = fastExp(ciphertext, d, n)
     pt_bytes = pt_decrypt.to_bytes(pt_decrypt.bit_length(), 'big')
     print(pt_bytes.decode('utf-8'))
 
-def smallTest():
+def part2():
     print("generating primes...")
-    p = getPrime(8) # 2048 bit primes
+    p = getPrime(2048) # 2048 bit primes
     q = p
     while (q == p):
-        q = getPrime(8)
-
-    n = p * q
+        q = getPrime(2048)
+    n, e = p * q, E
     euler_totient = (p-1) * (q-1)
-    print(p, q)
-
-    pub_key = (E, n)
-    # get multiplicative inverse d
-    d = abs(tableAlg(E, euler_totient))
-    print(findInverse(E, euler_totient))
-    print("d: ", d)
-    pri_key = (d, n)
-
-    plaintext = "h"
-    # str->bytes->int
+    d = abs(tableAlg(e, euler_totient))
     
-    pt_as_number = int.from_bytes(plaintext.encode('utf-8'), "big")
-    print(pt_as_number)
+    # bob computing message
+    bob_s = int.from_bytes("asdf".encode('utf-8'), "big")
+    bob_key = hashlib.sha256(bytes(bob_s)).digest()
+    c = fastExp(bob_s, e, n)
 
-    ciphertext = fastExp(pt_as_number, E, n) #4096 -> 4096 13^65537 mod n --> ct <= n
-    print("ct: ", ciphertext)
+    # mallory intercept
+    c = c^c
+    iv = b"0123456789012345"
+    mallory_key = hashlib.sha256(bytes(c)).digest()
+    malloryCipher = AES.new(mallory_key, AES.MODE_CBC, iv)
 
-
-    print("ct bitlength: ", ciphertext.bit_length(), "\nd bitlength: ", d.bit_length(), "\nn bitlength: ", n.bit_length())
+    # alice receive
+    alice_s = fastExp(c, d, n)
+    alice_key = hashlib.sha256(bytes(alice_s)).digest()
+    alice_m = b"Hi Bob!"
+    print("Alice wants to say: ")
+    print(pad_128_bits(alice_m))
     
-    #Above is Good.
+    aesCipher = AES.new(alice_key, AES.MODE_CBC, iv) #alice_key = bob_key = mallory_key
+    pt_pad = pad_128_bits(alice_m)
+    ct = aesCipher.encrypt(pt_pad)
 
-    pt_decrypt = fastExp(ciphertext, d, n) #(2^4096)^(2^4096) (13^65537)^(2^4096) => 2^4096 operations
-    print("pt_decrypt: ", pt_decrypt)
-    print("dh_proc: ", dh_proc(ciphertext, d, n))
-    # int -> bytes
-    pt_bytes = pt_decrypt.to_bytes(pt_decrypt.bit_length(), "big")
-    print(pt_bytes, pt_decrypt)
-    print(pt_bytes.decode('utf-8'))
+    #show Mallory's evil
+    print("Mallory can decrypt this: ")
+    print(malloryCipher.decrypt(ct))
 
-def expTest():
-    p = getPrime(2048) # 2048 bit primes
-    q = getPrime(2048)
-    print(fastExp(p, q, p*q))
 
-def tableTest():
-    p = 239
-    q = 151
-    euler_totient = (p-1) * (q-1)
-    p, q = E, euler_totient
 
-    # pub_key = (E, n)
-    print(extEuclidAlg(p, q)[0])
-    print(findInverse(p, q))
-    # print("d: ", d)
-    print(tableAlg(p, q))
-    
 sys.setrecursionlimit(4100)
-while(True):
-    test()
+part2()
